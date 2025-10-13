0e3bf1a5250b4b56d6a0b8ae08df9a0a68576a72
Jason Zeng <jason.zeng151@gmail.com>
2025-10-12 22:16:54 -0400

    layout: initial header and navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from "../components/Layout/Header/Header";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -24,11 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      helol
+      <body>
+        <Header />
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
---
+++ src/components/Layout/Header/Header.tsx
@@ -0,0 +1,46 @@
+export function Header() {
+   const navItems = [
+     { label: "Menu", href: "#menu"},
+     { label: "Plan", href: "#plan" },
+     { label: "History", href: "#history" },
+     { label: "Suggest", href: "#suggest" },
+     { label: "Recipes", href: "#Recipes" },
+     { label: "Friends", href: "#friends" },
+   ]
+
+  return (
+    <nav className="bg-(--color-header-bg) text-(--color-text-dark) shadow-md shadow-(--color-shadow)">
+      <div className="flex justify-between items-center py-4">
+        {/* Title */}
+        <div className="flex items-center ml-5">
+          <a
+            href="#home"
+            className="text-2xl font-bold"
+          >
+            Calorie Buddie
+          </a>
+        </div>
+
+        {/* Navbar */}
+        <div className="hidden md:flex items-center space-x-8">
+          {navItems.map((item, index) => (
+            <a
+              key={index}
+              href={item.href}
+              className={`px-3 py-2 rounded-md text-sm font-medium hover:text-[#ffffff] hover:bg-(--color-text-hover) transition-colors duration-200`}
+            >
+              {item.label}
+            </a>
+          ))}
+        </div>
+
+        {/* Login button */}
+        <div className="hidden md:block">
+          <button className="bg-(--color-footer-bg) hover:bg-[#75594a] text-#1a1a1a px-6 py-2 rounded-lg font-medium transition-colors duration-200 shadow-md hover:shadow-lg mr-5">
+            Log in
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
